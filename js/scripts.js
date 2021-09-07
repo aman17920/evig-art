fa77ca8667b46907892e1e9d7df50c88a21772fd
abhim42 <abhilashamishra777@gmail.com>
2021-09-07 18:29:17 +0530

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -44,48 +44,12 @@ var swiper = new Swiper(".mySwiper", {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  },
-});
-
-var swiper = new Swiper(".colorPencilSwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  autoSlider: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  // autoplay: {
-  //   delay: 5000,
-  // },
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
       slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
       spaceBetween: 30,
     },
     // when window width is >= 640px
